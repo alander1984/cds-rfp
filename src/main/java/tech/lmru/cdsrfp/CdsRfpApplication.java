995b2f0028c5e320bdd84243e77356183d426c0c
Alander <a.hrenov@norbit.ru>
2019-03-07 10:37:01 +0000

    Initial commit

---
+++ src/main/java/tech/lmru/cdsrfp/CdsRfpApplication.java
@@ -0,0 +1,13 @@
+package tech.lmru.cdsrfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdsRfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdsRfpApplication.class, args);
+	}
+
+}
