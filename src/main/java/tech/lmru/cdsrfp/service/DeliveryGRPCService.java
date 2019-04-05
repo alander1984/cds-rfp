afc8245797fcf50c8220294fbdf957a9b208ebe1
Sergey Tkachenko <s.tkachenko@norbit.ru>
2019-04-05 17:04:55 +0000

    DeliveryGRPCService

--- src/main/java/tech/lmru/cdsrfp/service/DeliveryGRPCService.java
+++ src/main/java/tech/lmru/cdsrfp/service/DeliveryGRPCService.java
@@ -2,9 +2,11 @@ package tech.lmru.cdsrfp.service;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
+import tech.lmru.cdsrfp.service.Delivery.Builder;
 import tech.lmru.grpc.GRPCService;
 import tech.lmru.repo.DeliveryRepository;
 
@@ -21,7 +23,68 @@ public class DeliveryGRPCService extends
         public void readAllDelivery(tech.lmru.cdsrfp.service.Empty request,
         io.grpc.stub.StreamObserver<tech.lmru.cdsrfp.service.DeliveryAllResponse> responseObserver) {
             List<tech.lmru.entity.order.Delivery> newDel = deliveryRepository.newDeliveries();
+            DeliveryAllResponse response = null;
+            if (newDel.isEmpty()) {
+                response = DeliveryAllResponse.newBuilder().build();
+            } else {
+                List<Delivery> deliveries = newDel.stream().map(delivery ->{
+                    User u = User.newBuilder()
+                    .setId(delivery.getCreateUser().getId())
+                    .setName(delivery.getCreateUser().getName()).build();
+                    User ul = User.newBuilder()
+                    .setId(delivery.getLastUpdateUser().getId())
+                    .setName(delivery.getLastUpdateUser().getName()).build();
+                    Store s =  Store.newBuilder()
+                    .setId(delivery.getStore().getId())
+                    //.setType(s.getType())
+                    .setName(delivery.getStore().getName())
+                    .setAddress(delivery.getStore().getAddress())
+                    .setCode(delivery.getStore().getCode())
+                    .setLon(delivery.getStore().getLon().doubleValue())
+                    .setLat(delivery.getStore().getLat().doubleValue())
+                    .setComment(delivery.getStore().getComment()).build();
+                    //DeliveryStatusEnum st;
+                    Builder build = Delivery.newBuilder()
+                        .setId(delivery.getId())
+                        .setLat(delivery.getLat().doubleValue())
+                        .setLon(delivery.getLon().doubleValue())
+                        .setTimeWindow(delivery.getTimeWindow())
+                        .setCreateUser(u)
+                        .setCreatedDate(delivery.getCreatedDate().toString())
+                        .setLastUpdateUser(ul)
+                        .setLastUpdateDate(delivery.getLastUpdateDate().toString())
+                        //.setStatus(st)
+                        .setDeliveryDateMin(delivery.getDeliveryDateMin().toString())
+                        .setDeliveryDateMax(delivery.getDeliveryDateMax().toString())
+                        .setComment(delivery.getComment())
+                        .setCity(delivery.getCity())
+                        .setStreet(delivery.getStreet())
+                        .setHouse(delivery.getHouse())
+                        .setEntrance(delivery.getEntrance())
+                        .setFlat(delivery.getEntrance())
+                        .setStore(s)
+                        .addAllItems(delivery.getItems().stream().map(item ->{
+                                return tech.lmru.cdsrfp.service.DeliveryItem.newBuilder()
+                                    .setId(item.getId())
+                                    .setProductLMCode(item.getProductLMCode())
+                                    .setProductLMName(item.getProductLMName())
+                                    .setWeight(item.getWeight().doubleValue())
+                                    .setVolume(item.getVolume().doubleValue())
+                                    .setWidth(item.getWidth().doubleValue())
+                                    .setLength(item.getLength().doubleValue())
+                                    .setQuantity(item.getQuantity().doubleValue())
+                                    .setLoadedQuantity(item.getLoadedQuantity().doubleValue())
+                                    .setApprovedQuantity(item.getApprovedQuantity().doubleValue())
+                                    //.setStatus(st)
+                                    .build();
+                        }).collect(Collectors.toList()));  
+                        return build.build();
+                }).collect(Collectors.toList());
+                response = DeliveryAllResponse.newBuilder()
+                    .addAllDeliveries(deliveries).build();
+            }
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
             
-      
     }
 }
