75dd696762360b3a3a39a221bae42ee4941114bf
Sergey Tkachenko <s.tkachenko@norbit.ru>
2019-04-10 11:45:13 +0000

    DeliveryService

--- src/main/java/tech/lmru/cdsrfp/service/DeliveryGRPCService.java
+++ src/main/java/tech/lmru/cdsrfp/service/DeliveryGRPCService.java
@@ -3,9 +3,12 @@ package tech.lmru.cdsrfp.service;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
+import javax.persistence.EntityNotFoundException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.lmru.cdsrfp.service.Delivery.Builder;
@@ -26,7 +29,7 @@ public class DeliveryGRPCService extends
         @Override
         public void readAllDelivery(tech.lmru.cdsrfp.service.Empty request,
         io.grpc.stub.StreamObserver<tech.lmru.cdsrfp.service.DeliveryAllResponse> responseObserver) {
-            List<tech.lmru.entity.order.Delivery> newDel = deliveryRepository.findAll();
+            List<tech.lmru.entity.order.Delivery> newDel = deliveryRepository.findAll().stream().filter(d -> (d.getStatus() == tech.lmru.entity.order.DeliveryStatusEnum.NEW)).collect(Collectors.toList());
             logger.info("List size of all deliveries {}", newDel.size());
             DeliveryAllResponse response = null;
             if (newDel.isEmpty()) {
@@ -34,71 +37,88 @@ public class DeliveryGRPCService extends
             } else {
                 List<Delivery> deliveries = newDel.stream().map(delivery ->{
                     //logger.info("List size of all deliveries.items {}", delivery.getItems().size());
-                    
-                    //tech.lmru.entity.order.Delivery delivery = deliveryRepository.findOneWithItemsById(1L);
-                    //logger.info("List size of all deliveries.items {}", delivery.getItems().size());
-                    User u = User.newBuilder()
-                    .setId(delivery.getCreateUser().getId())
-                    .setName(delivery.getCreateUser().getName()).build();
-                    User ul = User.newBuilder()
-                    .setId(delivery.getLastUpdateUser().getId())
-                    .setName(delivery.getLastUpdateUser().getName()).build();
-                    Store s =  Store.newBuilder()
-                    .setId(delivery.getStore().getId())
-                    //.setType(s.getType())
-                    .setName(delivery.getStore().getName())
-                    .setAddress(delivery.getStore().getAddress())
-                    .setCode(delivery.getStore().getCode())
-                    .setLon(delivery.getStore().getLon().doubleValue())
-                    .setLat(delivery.getStore().getLat().doubleValue())
-                    .setComment(delivery.getStore().getComment()).build();
-                    //DeliveryStatusEnum st;
-                    logger.info("Users, Store builded");
-                    Builder build = Delivery.newBuilder()
-                        .setId(delivery.getId())
-                        .setLat(delivery.getLat().doubleValue())
-                        .setLon(delivery.getLon().doubleValue())
-                        .setTimeWindow(delivery.getTimeWindow())
-                        .setCreateUser(u)
-                        .setCreatedDate(delivery.getCreatedDate().toString())
-                        .setLastUpdateUser(ul)
-                        .setLastUpdateDate(delivery.getLastUpdateDate().toString())
-                        //.setStatus(st)
-                        .setDeliveryDateMin(delivery.getDeliveryDateMin().toString())
-                        .setDeliveryDateMax(delivery.getDeliveryDateMax().toString())
-                        .setComment(delivery.getComment())
-                        .setCity(delivery.getCity())
-                        .setStreet(delivery.getStreet())
-                        .setHouse(delivery.getHouse())
-                        .setEntrance(delivery.getEntrance())
-                        .setFlat(delivery.getEntrance())
-                        .setStore(s)
-                        .addAllItems(delivery.getItems().stream().map(item ->{
-                            logger.info("Items building");
-                                return tech.lmru.cdsrfp.service.DeliveryItem.newBuilder()
-                                    .setId(item.getId())
-                                    .setProductLMCode(item.getProductLMCode())
-                                    .setProductLMName(item.getProductLMName())
-                                    .setWeight(item.getWeight().doubleValue())
-                                    .setVolume(item.getVolume().doubleValue())
-                                    .setWidth(item.getWidth().doubleValue())
-                                    .setLength(item.getLength().doubleValue())
-                                    .setQuantity(item.getQuantity().doubleValue())
-                                    .setLoadedQuantity(item.getLoadedQuantity().doubleValue())
-                                    .setApprovedQuantity(item.getApprovedQuantity().doubleValue())
-                                    //.setStatus(st)
-                                    .build();
-                        }).collect(Collectors.toList()));
-                        logger.info("All Items Added");
-                        return build.build();
+                        User u = User.newBuilder()
+                        .setId(delivery.getCreateUser().getId())
+                        .setName(delivery.getCreateUser().getName()).build();
+                        User ul = User.newBuilder()
+                        .setId(delivery.getLastUpdateUser().getId())
+                        .setName(delivery.getLastUpdateUser().getName()).build();
+                        Store s =  Store.newBuilder()
+                        .setId(delivery.getStore().getId())
+                        //.setType(s.getType())
+                        .setName(delivery.getStore().getName())
+                        .setAddress(delivery.getStore().getAddress())
+                        .setCode(delivery.getStore().getCode())
+                        .setLon(delivery.getStore().getLon().doubleValue())
+                        .setLat(delivery.getStore().getLat().doubleValue())
+                        .setComment(delivery.getStore().getComment()).build();
+                        //DeliveryStatusEnum st;
+                        logger.info("Users, Store builded");
+                        Builder build = Delivery.newBuilder()
+                            .setId(delivery.getId())
+                            .setLat(delivery.getLat().doubleValue())
+                            .setLon(delivery.getLon().doubleValue())
+                            .setTimeWindow(delivery.getTimeWindow())
+                            .setCreateUser(u)
+                            .setCreatedDate(delivery.getCreatedDate().toString())
+                            .setLastUpdateUser(ul)
+                            .setLastUpdateDate(delivery.getLastUpdateDate().toString())
+                            //.setStatus(st)
+                            .setDeliveryDateMin(delivery.getDeliveryDateMin().toString())
+                            .setDeliveryDateMax(delivery.getDeliveryDateMax().toString())
+                            .setComment(delivery.getComment())
+                            .setCity(delivery.getCity())
+                            .setStreet(delivery.getStreet())
+                            .setHouse(delivery.getHouse())
+                            .setEntrance(delivery.getEntrance())
+                            .setFlat(delivery.getEntrance())
+                            .setStore(s);
+                            
+                            return build.build();
+            
                 }).collect(Collectors.toList());
-               //List<Delivery> deliveries = new ArrayList<>();
-               //deliveries.add(build.build());
                 response = DeliveryAllResponse.newBuilder()
                     .addAllDeliveries(deliveries).build();
             }
             responseObserver.onNext(response);
             responseObserver.onCompleted();
             
-    }
+        }
+    
+        @Override
+        public void readAllDeliveryItemForDeliveryById(tech.lmru.cdsrfp.service.DeliveryIdRequest request,
+            io.grpc.stub.StreamObserver<tech.lmru.cdsrfp.service.DeliveryItemAllResponse> responseObserver) {
+            Long id = request.getId();
+            Optional<tech.lmru.entity.order.Delivery> byId = deliveryRepository.findById(id);
+            DeliveryItemAllResponse response = null;
+            if (byId.isPresent()) {
+              tech.lmru.entity.order.Delivery delivery = byId.get();
+              logger.info("Items Size: " + delivery.getItems().size());
+              List<tech.lmru.cdsrfp.service.DeliveryItem> di = delivery.getItems().stream().map(item ->{
+                                //logger.info("Items building");
+                                    return tech.lmru.cdsrfp.service.DeliveryItem.newBuilder()
+                                        .setId(item.getId())
+                                        .setProductLMCode(item.getProductLMCode())
+                                        .setProductLMName(item.getProductLMName())
+                                        .setWeight(item.getWeight().doubleValue())
+                                        .setHeight(item.getHeight().doubleValue())
+                                        .setWidth(item.getWidth().doubleValue())
+                                        .setLength(item.getLength().doubleValue())
+                                        .setQuantity(item.getQuantity().doubleValue())
+                                        .setLoadedQuantity(item.getLoadedQuantity().doubleValue())
+                                        .setApprovedQuantity(item.getApprovedQuantity().doubleValue())
+                                        //.setStatus(st)
+                                        .build();
+                            }).collect(Collectors.toList());
+             response =  DeliveryItemAllResponse.newBuilder().addAllItems(di).build();
+             responseObserver.onNext(response);
+             responseObserver.onCompleted();
+            }else {
+                responseObserver.onCompleted();
+                throw new EntityNotFoundException();
+            }
+            
+            
+        }
+
 }
