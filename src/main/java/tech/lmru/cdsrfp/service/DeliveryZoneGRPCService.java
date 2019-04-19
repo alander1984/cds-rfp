146ed5791fdcec8dc104827c9511792e9dab08d8
sandro <zaynukov@gmail.com>
2019-04-19 11:11:32 +0300

    Delete comments and write full path to classes

--- src/main/java/tech/lmru/cdsrfp/service/DeliveryZoneGRPCService.java
+++ src/main/java/tech/lmru/cdsrfp/service/DeliveryZoneGRPCService.java
@@ -1,13 +1,14 @@
 package tech.lmru.cdsrfp.service;
 
 import io.grpc.stub.StreamObserver;
-import tech.lmru.entity.deliveryzone.DeliveryZoneCoordinate;
 import tech.lmru.grpc.GRPCService;
 import tech.lmru.repo.DeliveryZoneCoordinateRepository;
 import tech.lmru.repo.DeliveryZoneRepository;
 
 import javax.inject.Inject;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @GRPCService
 public class DeliveryZoneGRPCService
@@ -31,16 +32,16 @@ public class DeliveryZoneGRPCService
      * @param responseObserver empty response
      */
     @Override
-    public void createDeliveryZone(DeliveryZone request, StreamObserver<Empty> responseObserver) {
+    public void createDeliveryZone(tech.lmru.cdsrfp.service.DeliveryZone request, StreamObserver<Empty> responseObserver) {
 //        if (!checkDeliveryZoneToCreate(request)) return;
 
         tech.lmru.entity.deliveryzone.DeliveryZone dz;
         dz = new tech.lmru.entity.deliveryzone.DeliveryZone();
 
-        for (CoordinateItem item : request.getCoordinateList()) {
-            dz.addCoordinate(new DeliveryZoneCoordinate(
-                    item.getLon(), item.getLat(), item.getPos()
-            ));
+        for (tech.lmru.cdsrfp.service.CoordinateItem item : request.getCoordinateList()) {
+            dz.addCoordinate(new tech.lmru.entity.deliveryzone.DeliveryZoneCoordinate(
+                    item.getLon(), item.getLat(), item.getPos())
+            );
         }
 
         deliveryZoneRepository.save(dz);
@@ -56,19 +57,20 @@ public class DeliveryZoneGRPCService
      * @param responseObserver empty response
      */
     @Override
-    public void updateDeliveryZone(DeliveryZone request, StreamObserver<Empty> responseObserver) {
-//        if (!checkDeliveryZoneToUpdate(request)) return;
-
+    public void updateDeliveryZone(tech.lmru.cdsrfp.service.DeliveryZone request, StreamObserver<Empty> responseObserver) {
         Optional<tech.lmru.entity.deliveryzone.DeliveryZone> res = deliveryZoneRepository.findById(request.getId());
         if (!res.isPresent()) return;
         tech.lmru.entity.deliveryzone.DeliveryZone thatDz = res.get();
 
         deliveryZoneCoordinateRepository.deleteAll(thatDz.getCoordinateList());
 
-        List<DeliveryZoneCoordinate> newList = new ArrayList<>(request.getCoordinateCount());
-        for (CoordinateItem item : request.getCoordinateList()) {
-            DeliveryZoneCoordinate c;
-            c = new DeliveryZoneCoordinate(item.getLon(), item.getLat(), item.getPos());
+        List<tech.lmru.entity.deliveryzone.DeliveryZoneCoordinate> newList;
+        newList = new ArrayList<>(request.getCoordinateCount());
+        for (tech.lmru.cdsrfp.service.CoordinateItem item : request.getCoordinateList()) {
+            tech.lmru.entity.deliveryzone.DeliveryZoneCoordinate c;
+            c = new tech.lmru.entity.deliveryzone.DeliveryZoneCoordinate(
+                    item.getLon(), item.getLat(), item.getPos()
+            );
             c.setDeliveryZone(thatDz);
 
             newList.add(c);
@@ -79,33 +81,4 @@ public class DeliveryZoneGRPCService
         responseObserver.onNext(Empty.getDefaultInstance());
         responseObserver.onCompleted();
     }
-
-    /*private boolean checkDeliveryZoneToCreate(DeliveryZone request) {
-        return request != null
-                && request.getId() == 0L
-                && checkCoordinateList(request.getCoordinateList());
-    }
-
-    private boolean checkDeliveryZoneToUpdate(DeliveryZone request) {
-        return request != null && checkCoordinateList(request.getCoordinateList());
-    }
-
-    private boolean checkCoordinateList(List<CoordinateItem> list) {
-        if (list == null || list.size() < 3) return false;
-
-        List<tech.lmru.cdsrfp.service.CoordinateItem> itemList;
-        itemList = list instanceof ArrayList ? list : new ArrayList<>(list);
-
-        itemList.sort(Comparator.comparingInt(CoordinateItem::getPos));
-
-        final double z = 0d;
-        for (int size = itemList.size(), i = size - 1; i >= 0; i--) {
-            CoordinateItem that = itemList.get(i);
-            if (that.getPos() != i && that.getLon() != z && that.getLat() != z) {
-                return false;
-            }
-        }
-        return true;
-    }*/
-
 }
